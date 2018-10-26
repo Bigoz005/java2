17164f179f9da55a67b66359d548c1b821d6368e
Bigoz005 <32684265+Bigoz005@users.noreply.github.com>
2018-10-26 09:49:03 +0200

    Update Main.java

--- cycle/src/com/company/Main.java
+++ cycle/src/com/company/Main.java
@@ -5,44 +5,59 @@ import java.io.*;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.Timer;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        URL link = new URL("http://szgrabowski.kis.p.lodz.pl/zpo18/nazwiska.txt ");
+
         BufferedReader in = null;
+        in = new BufferedReader(new InputStreamReader(link.openStream()));
 
-        URL nazwiska = new URL("http://szgrabowski.kis.p.lodz.pl/zpo18/nazwiska.txt ");
-        in = new BufferedReader(new InputStreamReader(nazwiska.openStream()));
-
-        int totalLines = 0;
+        int totalLines = 0; // zapisanie ilosci wszystkich liń
         while ((in.readLine()) != null) {
             totalLines++;
         }
-        in.close();
-        System.out.println("elo");
-        in = new BufferedReader(new InputStreamReader(nazwiska.openStream()));
 
-        Random random = new Random();
-        int randomInt = random.nextInt(totalLines);
-        int count = 0;
+        in.close();
+
+        in = new BufferedReader(new InputStreamReader(link.openStream()));
+
+        String[] nazwiska = new String[totalLines];
+        for (int i = 0; i < totalLines; i++) {
+            nazwiska[i] = in.readLine();
+        }
 
         Set<String> competitors = new HashSet<String>();
 
-        String icaocode;
+        String linia;
+        int count = 0;
+        Random random = new Random();
+        int randomInt;
 
-            while (competitors.size() < 15) {
-                icaocode = in.readLine();
-                if (count == randomInt) {
-                    System.out.println("elo");
-                    competitors.add((String) (icaocode));
-                }
-                count++;
-            }
-
-
-        System.out.print(competitors);
+        while (competitors.size() < 15) {
+            randomInt = random.nextInt(totalLines);
+            linia = nazwiska[randomInt];
+            competitors.add((String) (linia));
+            count++;
+        }
         in.close();
 
+        System.out.println("Uczestnikow: " + count);
+        System.out.println(competitors);
+
+        Timer timer = new Timer();
+        int sec = (int) (random.nextGaussian() * 40 + 300);
+        System.out.println(sec);
+        //1s rzeczywsta = 25s symulacji
+        if (sec < 250) {
+            sec = 250;
+        }
+        if (sec > 370) {
+            sec = 370;
+        }
+        System.out.println(sec);
     }
 }
 
