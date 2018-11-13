b30d9d6b60de78ae4ff765287d21b178e509972b
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-11-13 14:23:04 +0100

    Dodanie Levensteina i GUI

---
+++ 2.4.2/src/com/company/ReadFile.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+
+    String odczyt() {
+        String json = null;
+        BufferedReader fileReader = null;
+
+        try {
+            try {
+                fileReader = new BufferedReader(new FileReader("PolEngTest.json"));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            try {
+                json = fileReader.readLine();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return json;
+    }
+}
---
+++ 2.4.2/src/com/company/WriteFile.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFile {
+
+    void zapis(String json, String imie, String nazwisko) {
+
+        String nazwaPliku = imie + "_" + nazwisko + ".json";
+        FileWriter fileWriter = null;
+
+        try {
+            try {
+                fileWriter = new FileWriter(nazwaPliku);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            try {
+                fileWriter.write(json);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        } finally {
+            if (fileWriter != null) {
+                try {
+                    fileWriter.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
