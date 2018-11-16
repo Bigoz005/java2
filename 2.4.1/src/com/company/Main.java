4c0fb0e18339f61a03e47b2e57349f9b61caa40e
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-11-16 09:24:52 +0100

    Do oddania
    poprawione pierwsze sortowanie

--- 2.4.1/src/com/company/Main.java
+++ 2.4.1/src/com/company/Main.java
@@ -11,28 +11,49 @@ public class Main {
         System.out.println("Hello World!");
         String[] correctNames = {"Agnieszka", "Darek", "Łukasz", "Órszula", "Stefania", "Ścibor", "Świętopełk", "Zyta"};
         String[] names = {"Łukasz", "Ścibor", "Stefania", "Darek", "Agnieszka", "Zyta", "Órszula", "Świętopełk"};
-        sortStrings(Collator.getInstance(new Locale("pl")), names);
-        fastSortStrings(Collator.getInstance(new Locale("pl")), names);
-        fastSortStrings2(Collator.getInstance(new Locale("pl")), names);
+        sortStrings(Collator.getInstance(new Locale("pl", "PL")), names);
+        fastSortStrings(Collator.getInstance(new Locale("pl", "PL")), names);
+        fastSortStrings2(Collator.getInstance(new Locale("pl", "PL")), names);
     }
 
-    //do poprawy
+    Collator c = Collator.getInstance(new Locale("pl", "PL"));
+
+
     public static void sortStrings(Collator collator, String[] words) {
-        int i;
-        for (i = 1; i < words.length; i++)
-        {
-            String temp;
+        int i, j;
+        String temp;
+        for (i = 1; i < words.length; i++) {
             temp = words[i];
-            int j;
-            for (j = i; j > 0; j--)
-                if (temp.compareTo(words[j - 1]) < 0)
-                    words[j] = words[j - 1];
-                else
-                    break;
-            words[j] = temp;
+            for (j = i - 1; j >= 0 && (collator.compare(words[j], temp) == 1); j--) {
+                words[j + 1] = words[j];
+            }
+            words[j + 1] = temp;
         }
     }
 
+
+    // z anonimowym komparatorem
+    public static void fastSortStrings(Collator collator, String[] words) {
+
+        // tez zadziala bo Collator implementuje Comparator
+        // Arrays.sort(words, collator);
+
+        Arrays.sort(words, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return collator.compare(s1, s2);
+            }
+        });
+    }
+
+    // z lambda
+    public static void fastSortStrings2(Collator collator, String[] words) {
+
+        Arrays.sort(words, (String s1, String s2) -> {
+            return collator.compare(s1, s2);
+        });
+    }
+}
 
     // z anonimowym komparatorem
     public static void fastSortStrings(Collator collator, String[] words) {
