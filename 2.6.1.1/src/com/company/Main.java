5b3053277a9b371b8bfa56f1c26728427d1fdd57
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-12-05 00:08:35 +0100

    Do oddania

--- 2.6.1.1/src/com/company/Main.java
+++ 2.6.1.1/src/com/company/Main.java
@@ -2,34 +2,30 @@ package com.company;
 
 public class Main{
     public static void main(String[] args) {
-        Publication b1 = new Book("Adam Mickiewicz", "Pan Tadeusz", 340);
-        Publication b2 = new Book("Adam Mickiewicz", "Dziady", 130);
+        Publication b1 = new Book("Mario Puzo", "Ojciec Chrzestny", 480);
+        Publication b2 = new Book("Bruce Eckel", "Thinking In Java", 1200);
 
-        Publication bb1 = new BookWithNormalCover(b1);
-        Publication bb2 = new BookWithHardCover(b2);
+        Publication bb1 = new WithNormalCover(b1);
+        Publication bb2 = new WithHardCover(b2);
 
-        //Wyjątek - okładka może być tylko jedna!
-        //Publication fail = new BookWithHardCover(bb1);
+        //Publication fail = new WithHardCover(bb1);
 
-        //Wyjątek - nie można obłożyć obwolutą książki bez twardej okładki.
-        //Publication fakeBook = new BookWithWrapper(bb1);
+        //Publication fakeBook = new WithWrapper(bb1);
 
-        Publication bbb2 = new BookWithWrapper(bb2);
-        Publication test = new BookWithNormalCover(bbb2);
+        Publication bbb2 = new WithWrapper(bb2);
+        Publication test = new WithNormalCover(bbb2);
 
-        //Wyjątek - obwoluta może być tylko jedna.
-        //Publication reject = new BookWithWrapper(bbb2);
+        //Publication reject = new WithWrapper(bbb2);
 
-        Publication dziadyZAutografemWieszcza = new BookWithAuthorAutograph(bb2, "Drogiej Hani - Adam Mickiewicz");
+        Publication Salesman = new WithAutograph(bb2, "*Slaps cover of the book*");
 
-        //Wyjątek - autograf może być tylko jeden.
-        //Publication dziadyZDwomaAutografami = new BookWithAuthorAutograph(dziadyZAutografemWieszcza, "Haniu, to nieprawda, Dziady napisałem ja, Julek Słowacki!");
+        //Publication dziadyZDwomaAutografami = new WithAuthorAutograph(Salesman, "This bad boy can fit so many crimes!");
 
         System.out.println(b1.toString());
         System.out.println(b2.toString());
         System.out.println(bb1.toString());
         System.out.println(bb2.toString());
         System.out.println(bbb2.toString());
-        System.out.println(dziadyZAutografemWieszcza.toString());
+        System.out.println(Salesman.toString());
     }
 }
---
+++ 2.6.1.1/src/com/company/WithWrapper.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class WithWrapper extends BookDekorator{
+
+    public WithWrapper(Publication decoratedBook) {
+        super(decoratedBook);
+        if(!decoratedBook.toString().contains("Okładka twarda")) {
+            try {
+                throw new NoHardCoverException();
+            } catch (NoHardCoverException e) {
+                System.out.println("Nie można obłożyć obwolutą książki, bez twardej okładkii!");
+            }
+            System.exit(0);
+        }
+        if(decoratedBook.toString().contains("Okładka z obwolutą")) {
+            try {
+                throw new NoMoreThanOneWrapperException();
+            } catch (NoMoreThanOneWrapperException ne) {
+                System.out.println("Obwoluta może być tylko jedna!");
+            }
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.decoratedBook + " | " + "Okładka z obwolutą" + " | ";
+    }
+}
