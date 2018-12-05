5371c2b9128bc5a5d79fb6265a81376ba1498a84
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-12-05 01:07:55 +0100

    Delete NoMoreThanOneAuthorException.java

--- 2.6.1.1/src/com/company/NoMoreThanOneAuthorException.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class NoMoreThanOneAuthorException extends Exception {
-    public NoMoreThanOneAuthorException() {}
-
-    public NoMoreThanOneAuthorException(String message) {
-        super(message);
-    }
-}
