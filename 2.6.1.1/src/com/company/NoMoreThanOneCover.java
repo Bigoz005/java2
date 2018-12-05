86e3893e65290455c4fff47156f5cd9bb437c05d
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-12-05 01:08:01 +0100

    Delete NoMoreThanOneCover.java

--- 2.6.1.1/src/com/company/NoMoreThanOneCover.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class NoMoreThanOneCover extends Exception{
-    public NoMoreThanOneCover() {}
-
-    public NoMoreThanOneCover(String message) {
-        super(message);
-    }
-}
