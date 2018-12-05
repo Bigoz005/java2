dcee279348c1da4726324bcaddb716db3fa641e4
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-12-05 16:45:40 +0100

    Do oddania

--- 2.7.3/src/com/company/Main.java
+++ 2.7.3/src/com/company/Main.java
@@ -1,5 +1,5 @@
 package com.company;
-
+/*
 import javafx.application.Application;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Worker;
@@ -10,13 +10,14 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+*/
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main/* extends Application*/ {
     public static void main(String[] args){
         try {
-            RandomNumbers test = new RandomNumbers(10000, 0, 1);
+            RandomNumbers test = new RandomNumbers(50, 0, 1);
             test.closeDataStreams();
         } catch (FileNotFoundException e){
             System.out.println(e.getMessage());
@@ -31,29 +32,21 @@ public class Main/* extends Application*/ {
         stage.setWidth(400);
         stage.setHeight(500);
         Scene scene = new Scene(new Group());
-
-
         final WebView browser = new WebView();
         final WebEngine webEngine = browser.getEngine();
-
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setContent(browser);
-
         webEngine.getLoadWorker().stateProperty()
                 .addListener(new ChangeListener<Worker.State>() {
                     @Override
                     public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
-
                         if (newState == Worker.State.SUCCEEDED) {
                             stage.setTitle(webEngine.getLocation());
                         }
-
                     }
                 });
         webEngine.load("https://www.youtube.com/watch?v=QHYwprmWMfY&feature=youtu.be");
-
         scene.setRoot(scrollPane);
-
         stage.setScene(scene);
         stage.show();
     }*/
