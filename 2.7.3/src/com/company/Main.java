91f1feec8605a5e48cc2e59660154ee0a59af571
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-12-05 12:57:55 +0100

    Do oddania juz z eksportem w xlsx // IT'S WEDNESDAY MY DUDES

--- 2.7.3/src/com/company/Main.java
+++ 2.7.3/src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.beans.value.ChangeListener;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Main {
+public class Main/* extends Application*/ {
     public static void main(String[] args){
         try {
             RandomNumbers test = new RandomNumbers(10000, 0, 1);
@@ -13,5 +23,38 @@ public class Main {
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
+        //launch(args);
     }
+/*
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setWidth(400);
+        stage.setHeight(500);
+        Scene scene = new Scene(new Group());
+
+
+        final WebView browser = new WebView();
+        final WebEngine webEngine = browser.getEngine();
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(browser);
+
+        webEngine.getLoadWorker().stateProperty()
+                .addListener(new ChangeListener<Worker.State>() {
+                    @Override
+                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
+
+                        if (newState == Worker.State.SUCCEEDED) {
+                            stage.setTitle(webEngine.getLocation());
+                        }
+
+                    }
+                });
+        webEngine.load("https://www.youtube.com/watch?v=QHYwprmWMfY&feature=youtu.be");
+
+        scene.setRoot(scrollPane);
+
+        stage.setScene(scene);
+        stage.show();
+    }*/
 }
--- 2.7.3/src/com/company/RandomNumbers.java
+++ 2.7.3/src/com/company/RandomNumbers.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import com.smartxls.ChartShape;
+import com.smartxls.WorkBook;
+
 import java.io.*;
 import java.text.NumberFormat;
 import java.util.Random;
@@ -13,8 +16,10 @@ public class RandomNumbers {
     FileWriter fileWriter;
     PrintWriter textOutputStream;
 
-    public RandomNumbers(int count, int mean, int standardDeviation)throws FileNotFoundException, IOException {
+
+    public RandomNumbers(int count, int mean, int standardDeviation) throws FileNotFoundException, IOException {
         assert (standardDeviation >= 0);
+        WorkBook workBook = new WorkBook();
         this.count = count;
         this.mean = mean;
         this.standardDeviation = standardDeviation;
@@ -22,10 +27,48 @@ public class RandomNumbers {
         this.binaryInputStream = new BufferedReader(new InputStreamReader(new FileInputStream("binout.dat")));
         this.fileWriter = new FileWriter("textout.txt");
         this.textOutputStream = new PrintWriter(getFileWriter());
+
         for (int i = 0; i < getCount(); i++) {
             double randomNumber = generateRandomNumber();
             String randomNumberString = format(randomNumber);
             writeLineToBinaryFile(randomNumberString);
+        }
+        try {
+            for (int row = 1; row <= getCount(); row++) {
+                double randomNumber = generateRandomNumber();
+                String randomNumberString = format(randomNumber);
+                workBook.setText(row, 1, randomNumberString);
+            }
+            for (int row = 2; row <= getCount()+1; row++) {
+                workBook.setFormula(row, 2, "Value($B$"+row+")");
+            }
+            workBook.insertSheets(0, 1);//insert sheet from left
+            workBook.setSheet(0);//select the new created sheet
+            workBook.setSheetName(0, "ChartSheet");
+
+            ChartShape chart = workBook.addChartSheet(0);//add chart
+            //Open your 3rd eye
+            //chart.setChartType(ChartShape.Radar);
+
+            chart.addSeries();
+            //chart.setSeriesName(0, "My Series number 1");
+
+            chart.setSeriesYValueFormula(0, "Sheet1!$C$3:$C$10002");
+            //chart.setSeriesXValueFormula(0, "Sheet1!$B$2:$B$10001");
+
+            //set axis title
+            chart.setAxisTitle(ChartShape.XAxis, 0, "X-axis data");
+            chart.setAxisTitle(ChartShape.YAxis, 0, "Y-axis data");
+
+            //set series name
+            chart.setSeriesName(0, "My Series number 1");
+
+            chart.setTitle("My Chart");
+
+            workBook.writeXLSX("Chartsheet.xlsx");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
         for (int i = 0; i < getCount(); i++) {
             String line = readLineFromBinaryFile();
@@ -33,14 +76,14 @@ public class RandomNumbers {
         }
     }
 
-    public void closeDataStreams() throws IOException{
+    public void closeDataStreams() throws IOException {
         getBinaryOutputStream().close();
         getBinaryInputStream().close();
         getFileWriter().close();
         getTextOutputStream().close();
     }
 
-    public void writeLineToBinaryFile(String number) throws IOException{
+    public void writeLineToBinaryFile(String number) throws IOException {
         getBinaryOutputStream().writeChars(number);
         getBinaryOutputStream().writeChar('\n');
     }
@@ -51,20 +94,20 @@ public class RandomNumbers {
         return result;
     }
 
-    public void writeLineToTextFile(int line, String number) throws IOException{
+    public void writeLineToTextFile(int line, String number) throws IOException {
         number = number.replaceAll("\u0000", "");
         getTextOutputStream().print(line + 1);
         getTextOutputStream().print("\u0009");
         getTextOutputStream().println(number);
     }
 
-    public double generateRandomNumber(){
+    public double generateRandomNumber() {
         Random r = new Random();
-        Double result = r.nextGaussian()*getStandardDeviation()+getMean();
+        Double result = r.nextGaussian() * getStandardDeviation() + getMean();
         return result;
     }
 
-    public String format(double number){
+    public String format(double number) {
         String result = NumberFormat.getInstance().format(number);
         return result;
     }
