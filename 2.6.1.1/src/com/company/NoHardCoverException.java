d7fc80f4414d580b807ca6ad75b8d28a494f3ec7
Michał Nawrot <32684265+Bigoz005@users.noreply.github.com>
2018-12-05 01:07:49 +0100

    Delete NoHardCoverException.java

--- 2.6.1.1/src/com/company/NoHardCoverException.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class NoHardCoverException extends Exception {
-    public NoHardCoverException() {}
-
-    public NoHardCoverException(String message) {
-        super(message);
-    }
-}
